4b3d8d959ec1e2dcdd1c7c57de68840f902e4fc3
Sebastian Vera <sebastian.vera@colegiochampagnat.com.ar>
2021-11-01 18:42:27 -0300

    python

---
+++ Python/index.py
@@ -0,0 +1,22 @@
+# https://www.online-python.com/
+
+number = 2
+
+def sum(a, b):
+    return a + b
+
+print("Hola mundo!")
+
+print(sum(3,4))
+
+def greater_than(a, b):
+    if a > b:
+        return a
+    else:
+        return b
+
+print("El mayor es " + str(greater_than(5,6)))
+
+# 4 funciones matematicas basicas
+# Funcion que retorne el MCM
+# Funcion que dado un numero n, imprima y retorne el n-avo numero de la secuencia fibonacci
