5feb51ef8740e6bd7e6bf4ee3ffadf4ec51e1b85
Sebastian Vera <sebastian.vera@colegiochampagnat.com.ar>
2021-11-08 18:49:04 -0300

    more of python

--- Python/index.py
+++ Python/index.py
@@ -1,13 +1,31 @@
 # https://www.online-python.com/
-
 number = 2
 
+def print_aux(operacion, a, b, result):
+    print("El resultado de la " + operacion + " entre " + str(a) + " y " + str(b) + " es " + str(result))
+
 def sum(a, b):
-    return a + b
+    result = a + b
+    print_aux("suma", a, b, result)
+    result
 
-print("Hola mundo!")
+def substraction(a, b):
+    result = a - b
+    print_aux("resta", a, b, result)
+    result
 
-print(sum(3,4))
+def times(a, b):
+    result = a * b
+    print_aux("multiplicacion", a, b, result)
+    result
+
+def division(a, b):
+    if b == 0:
+        print("Math error")
+        return "Math error"
+    result = float(a) / b
+    print_aux("division", a, b, result)
+    result
 
 def greater_than(a, b):
     if a > b:
@@ -15,8 +33,39 @@ def greater_than(a, b):
     else:
         return b
 
-print("El mayor es " + str(greater_than(5,6)))
+#print("El mayor es " + str(greater_than(5,6)))
 
-# 4 funciones matematicas basicas
+def fibonacci(n):
+    if n <= 0:
+        print("error")
+        return "error"
+    a = 0
+    b = 1
+    if n <= 2:
+        print(n-1)
+    aux = 0
+    print(0)
+    for i in range(n-1):
+        aux = b
+        b = a + b
+        a = aux
+        print(aux)
+
+
+
 # Funcion que retorne el MCM
-# Funcion que dado un numero n, imprima y retorne el n-avo numero de la secuencia fibonacci
+# Fibonacci recursivo
+
+#sum(5,7)
+#times(5,6)
+#substraction(8,9)
+#division(1,0)
+#division(8,3)
+#fibonacci(8)
+
+print(range(5))
+print(range(-2,5))
+print(range(3,6))
+print(range(1,10,2))
+print(range(10,1))
+print(range(1,10,-1))
